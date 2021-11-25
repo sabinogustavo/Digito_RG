7d6e2ec15a062b8ca124f7997eea5c850c0db3ee
Gustavo Sabino <sabino.gustavo.gs@gmail.com>
2021-11-25 08:52:15 -0300

    Created functions

--- digitoRG.py
+++ digitoRG.py
@@ -1,19 +1,21 @@
 entradaNumRG = input('insira o numero do seu RG sem pontos ou traços \n')
-listaEntrada = []
-multiplicador = 2
 
-for numero in entradaNumRG:
-    listaEntrada.append(int(numero)*multiplicador)
-    multiplicador = multiplicador + 1
+def multiplicadorEntrada(entradaNumRG):
+    listaEntrada = []
+    multiplicador = 2
+    for numero in entradaNumRG:
+        listaEntrada.append(int(numero)*multiplicador)
+        multiplicador = multiplicador + 1
+    return listaEntrada    
     
+def saidaDigitoVer(digitoVer):
+    if digitoVer == 10:
+        print ('O dígito verificador do seu rg é X')
+    elif digitoVer == 11:
+        print ('O dígito verificador do seu rg é 0')
+    else:
+        print('O dígito verificador do seu Rg é %d'%digitoVer)
 
-somaNumRG = sum(listaEntrada)
+digitoVer = 11 - (sum(multiplicadorEntrada(entradaNumRG))%11)
 
-digitoVer = 11 - (somaNumRG%11)
-
-if digitoVer == 10:
-    print ('O dígito verificador do seu rg é X')
-elif digitoVer == 11:
-    print ('O dígito verificador do seu rg é 0')
-else:
-    print('O dígito verificador do seu Rg é %d'%digitoVer)
+saidaDigitoVer(digitoVer)
